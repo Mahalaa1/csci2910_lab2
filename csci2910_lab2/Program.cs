a598a4a6e6d42ee62eabaed1ef8abc4aba0daf0e
Austin Mahala <mahalaa1@etsu.edu>
2023-05-17 17:58:08 -0400

    Added part 2 of lab 2 to ask the user for input of what number they want to create a multiplication table for and what they want to multiply by.

--- csci2910_lab2/Program.cs
+++ csci2910_lab2/Program.cs
@@ -1,4 +1,15 @@
-﻿namespace csci2910_lab2
+﻿/**
+*--------------------------------------------------------------------
+* File name: Program.cs
+* Project name: Lab2
+*--------------------------------------------------------------------
+* Author’s name and email: Austin Mahala, mahalaa1@etsu.edu
+* Course-Section: CSCI-2910-970
+* Creation Date: 5/17/2023
+* -------------------------------------------------------------------
+*/
+
+namespace csci2910_lab2
 {
     internal class Program
     {
@@ -6,6 +17,9 @@
         {
             // Part 1: Print the output of adding two numbers from user input.
             AddNum();
+
+            // Part 2: Take a user’s input number and input its multiplication table. The user can choose how high to multiply by.
+            MultiplicationTable();
         }
 
         public static void AddNum()
@@ -27,5 +41,24 @@
             Console.WriteLine();
         }
 
-    }
+        public static void MultiplicationTable()
+        {
+            Console.WriteLine("Now let's make a multiplication table.");
+            Console.WriteLine();
+
+            Console.Write("What number would you like to see the multiplication table for? ");
+            int tableNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("How high would you like to multiply by? ");
+            int tableLength = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i <= tableLength; i++)
+            {
+                int result = tableNumber * i;
+                Console.WriteLine($"{tableNumber} * {i} = {result}");
+            }
+
+        }
 }

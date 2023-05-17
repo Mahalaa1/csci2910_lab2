7d194f8476f881ac32b196c20c9a61a6f71abb8a
Austin Mahala <mahalaa1@etsu.edu>
2023-05-17 17:55:25 -0400

    Created part 1 for lab to which asks the user for to input two numbers to add together

--- csci2910_lab2/Program.cs
+++ csci2910_lab2/Program.cs
@@ -4,9 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            // Part 1: Print the output of adding two numbers from user input.
+            AddNum();
         }
 
-        
+        public static void AddNum()
+        {
+            Console.WriteLine("Let's add two numbers together.");
+            Console.WriteLine();
+
+            Console.Write("Enter a number to add: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Choose a second number to add: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            double sum = firstNumber + secondNumber;
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+            Console.WriteLine();
+        }
+
     }
 }
